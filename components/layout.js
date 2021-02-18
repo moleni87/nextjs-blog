7ea0e04825c07a2db49b49dc9b59ef5280f0211d
seo <moleni87@gmail.com>
2021-02-18 11:08:31 +0900

    update

--- components/layout.js
+++ components/layout.js
@@ -1,39 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import Alert, { type } from '../components/alert'
+import Head from "next/head"
+import Image from "next/image"
+import styles from "./layout.module.css"
+import utilStyles from "../styles/utils.module.css"
+import Link from "next/link"
+import Alert, { type } from "../components/alert"
 
-export const siteTitle = 'Next.js Sample Website'
+const name = "Your Name"
+export const siteTitle = "Next.js Sample Website"
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
         <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
+          name='description'
+          content='Learn how to build a personal website using Next.js'
         />
         <meta
-          property="og:image"
+          property='og:image'
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
       </Head>
       <header className={styles.header}>
         {home ? (
           <>
-            <Alert children="zzz" type="error">
+            <Alert children='zzz' type='error'>
               22
             </Alert>
             <Image
               priority
-              src="/images/profile.jpg"
+              src='/images/profile.jpg'
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,11 +44,11 @@ export default function Layout({ children, home }) {
           </>
         ) : (
           <>
-            <Link href="/">
+            <Link href='/'>
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src='/images/profile.jpg'
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
@@ -56,7 +57,7 @@ export default function Layout({ children, home }) {
               </a>
             </Link>
             <h2 className={utilStyles.headingLg}>
-              <Link href="/">
+              <Link href='/'>
                 <a className={utilStyles.colorInherit}>{name}</a>
               </Link>
             </h2>
@@ -66,7 +67,7 @@ export default function Layout({ children, home }) {
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
-          <Link href="/">
+          <Link href='/'>
             <a>← Back to home</a>
           </Link>
         </div>
